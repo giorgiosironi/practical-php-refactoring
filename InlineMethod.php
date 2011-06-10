000ae7339ccf440114db33d5976aff96465810be
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-06-10 16:50:35 +0200

    Initial state

---
+++ InlineMethod.php
@@ -0,0 +1,29 @@
+<?php
+class InlineMethodTest extends PHPUnit_Framework_TestCase
+{
+    public function testMethodExtractionShouldNotMakeThisTestFail()
+    {
+        $logParser = new LogParser();
+        $logLine = '127.0.0.1 - - [04/30/2011:17:07:31 +0200] "GET /favicon.ico HTTP/1.1" 404 450 "-" "Mozilla"';
+        $day = $logParser->getDayOfTheWeek($logLine);
+        $this->assertEquals('On Saturday we got a visit', $day);
+    }
+}
+
+class LogParser
+{
+    public function getDayOfTheWeek($logLine)
+    {
+        $date = $this->getDate($logLine);
+        return 'On ' . $date->format('l') . ' we got a visit';
+    }
+
+    /**
+     * @return DateTime
+     */
+    private function getDate($logLine)
+    {
+        preg_match('([0-9]{2}/[0-9]{2}/[0-9]{4})', $logLine, $matches);
+        return new DateTime($matches[0]);
+    }
+}
