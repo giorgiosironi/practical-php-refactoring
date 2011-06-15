651d5272e4b7294cb77ca4cb5177cad47249dd89
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-06-15 11:09:52 +0200

    Non-working implementation

--- InlineTemp.php
+++ InlineTemp.php
@@ -13,21 +13,40 @@ class AccountViewTest extends PHPUnit_Framework_TestCase
         $account = new AccountView(-20);
         $this->assertEquals('<div class="red">-20</div>', $account->render());
     }
+    
+    /**
+     * We add an allowed overdraft as parameter.
+     */
+    public function testIsDisplayedAsMixedWhenNotTooMuchNegative()
+    {
+        $account = new AccountView(-20, 1000);
+        $this->assertEquals('<div class="red">-20 (<span class="green">maximum overdraft: 1000</span>)</div>', $account->render());
+    }
 }
 
 class AccountView
 {
     private $total;
 
-    public function __construct($total)
+    public function __construct($total, $maximumOverdraft = 0)
     {
         $this->total = $total;
+        $this->maximumOverdraft = $maximumOverdraft;
     }
 
+    /**
+     * This implementation doesn't work: $negative have different meanings in 
+     * case of negative total or negative total larger than the maximum overdraft.
+     */
     public function render()
     {
         $negative = $this->total < 0;
         $class =  $negative ? 'red' : 'green';
-        return "<div class=\"$class\">$this->total</div>";
+        if ($negative && $this->maximumOverdraft) {
+            $overdraftNotice =" (<span class=\"$class\">maximum overdraft: $this->maximumOverdraft</span>)";
+        } else {
+            $overdraftNotice = '';
+        }
+        return "<div class=\"$class\">{$this->total}{$overdraftNotice}</div>";
     }
 }
