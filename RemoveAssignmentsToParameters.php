da79c4d9f015242838cf61f51f1306bd40943157
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-07-04 11:45:11 +0200

    Defined local variables

--- RemoveAssignmentsToParameters.php
+++ RemoveAssignmentsToParameters.php
@@ -12,18 +12,15 @@ class RemoveAssignmentsToParametersTest extends PHPUnit_Framework_TestCase
 class World
 {
     /**
-     * These docblock will be outdated after reading one line into the method.
+     * The local variable names serve as documentation now.
      * @param int $firstMeridian    from -180 to 180
      * @param int $secondMeridian   from -180 to 180
-     * I wrote this, but in a week I will hardly follow the logic inside.
-     * While in the case of Split Temporary Variable $date was still a DateTime
-     * object, here the meaning of $secondMeridian becomes completely different.
      */
     public function getDistanceBetweenMeridians($firstMeridian, $secondMeridian)
     {
-        $secondMeridian -= $firstMeridian;
-        $secondMeridian *= 60;
-        $secondMeridian *= 1.852;
-        return round($secondMeridian);
+        $degreesOfDifference = $secondMeridian - $firstMeridian;
+        $nauticalMiles = $degreesOfDifference * 60;
+        $distance = $nauticalMiles * 1.852;
+        return round($distance);
     }
 }
