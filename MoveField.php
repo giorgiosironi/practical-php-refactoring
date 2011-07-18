e1178c598020b6b5fcb95e707020b45ba78f43f3
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-07-18 09:28:37 +0200

    Initial state

---
+++ MoveField.php
@@ -0,0 +1,66 @@
+<?php
+class MoveMethodTest extends PHPUnit_Framework_TestCase
+{
+    public function testDisplayItsLinksInShortForm()
+    {
+        $tagCloud = new TagCloud(array(
+            new Link('http://giorgiosironi.blogspot.com/search/label/productivity', 'archives'),
+            new Link('http://giorgiosironi.blogspot.com/search/label/software%20development', 'archives')
+        ));
+        $html = $tagCloud->toHtml();
+        $this->assertEquals(
+            "<a href=\"http://giorgiosironi.blogspot.com/search/label/productivity\" rel=\"archives\">productivity</a>\n"
+          . "<a href=\"http://giorgiosironi.blogspot.com/search/label/software%20development\" rel=\"archives\">software development</a>\n",
+            $html
+        );
+    }
+}
+
+class TagCloud
+{
+    private $links;
+
+    public function __construct(array $links)
+    {
+        $this->links = $links;
+    }
+
+    public function toHtml()
+    {
+        $html = '';
+        foreach ($this->links as $link) {
+            $text = $link->text();
+            $rel = $link->getRel();
+            $html .= "<a href=\"$link\" rel=\"$rel\">$text</a>\n";
+        }
+        return $html;
+    }
+}
+
+class Link
+{
+    private $url;
+    private $rel;
+
+    public function __construct($url, $rel = null)
+    {
+        $this->url = $url;
+        $this->rel = $rel;
+    }
+
+    public function __toString()
+    {
+        return $this->url;
+    }
+
+    public function getRel()
+    {
+        return $this->rel;
+    }
+
+    public function text()
+    {
+        $lastFragment = substr(strrchr($this, '/'), 1);
+        return str_replace('%20', ' ', $lastFragment);
+    }
+}
