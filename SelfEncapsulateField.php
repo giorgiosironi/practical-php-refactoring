d136356f81c6e4eb4ece4c1d004cdc18568174d6
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-08-03 10:13:27 +0200

    Satisfied second requirement

--- SelfEncapsulateField.php
+++ SelfEncapsulateField.php
@@ -10,7 +10,6 @@ class SelfEncapsulateField extends PHPUnit_Framework_TestCase
 
     public function testParagraphIsDisplayedAsAlwaysImportant()
     {
-        $this->markTestSkipped();
         $p = new ImportantParagraph('Lorem ipsum');
         $expected = '<p class="important">Lorem ipsum</p>';
         $this->assertEquals($expected, $p->__toString());
@@ -36,5 +35,26 @@ class Paragraph
     protected function getClass()
     {
         return $this->class;
+    }
+}
+
+class ImportantParagraph extends Paragraph
+{
+    /**
+     * This constructor is an hack, but at least the Liskov Substitution 
+     * Principle is respected in the external interface, which only
+     * consists of __toString().
+     * If we want to improve this constructor, we can extract a common 
+     * abstract class between Paragraph and ImportantParagraph, so that
+     * both can have their own constructors.
+     */
+    public function __construct($text)
+    {
+        parent::__construct($text, null);
+    }
+   
+    protected function getClass()
+    {
+        return 'important';
     }
 }
