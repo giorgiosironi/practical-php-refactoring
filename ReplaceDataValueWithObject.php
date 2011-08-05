a2a04c8fa109b21c40978762d8cf2f2cfaa2207f
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-08-05 10:07:20 +0200

    Created class as extension of ArrayObject

--- ReplaceDataValueWithObject.php
+++ ReplaceDataValueWithObject.php
@@ -4,20 +4,24 @@ class ReplaceDataValueWithObject extends PHPUnit_Framework_TestCase
     public function testUserCanSetANewPassword()
     {
         $userService = new UserService(/* other dependencies*/);
-        $userService->newPassword(array(
+        $userService->newPassword(new ChangePasswordCommand(array(
             'userId' => 42,
             'oldPassword' => 'gismo',
             'newPassword' => 'supersecret',
             'repeatNewPassword' => 'supersecret'
-        ));
+        )));
         $this->markTestIncomplete('This refactoring is about the introduction of an object; it suffices that the test does not explode.');
     }
 }
 
 class UserService
 {
-    public function newPassword($changePasswordData)
+    public function newPassword(ChangePasswordCommand $changePasswordData)
     {
         /* it's not interesting to do something here */
     }
+}
+
+class ChangePasswordCommand extends ArrayObject
+{
 }
