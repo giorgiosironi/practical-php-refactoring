a53021e410fe08c03a941e9ec2b22ead27970e1c
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-08-22 10:31:26 +0200

    Introduced class with public field for the array

--- ReplaceArrayWithObject.php
+++ ReplaceArrayWithObject.php
@@ -3,10 +3,19 @@ class ReplaceArrayWithObjectTest extends PHPUnit_Framework_TestCase
 {
     public function testCanDefineAnHttpResponse()
     {
-        $response = array(
+        $response = new HttpResponse(array(
             'success' => true,
             'content' => '{someJson:"ok"}'
-        );
+        ));
     }
 }
 
+class HttpResponse
+{
+    public $data;
+
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+}
