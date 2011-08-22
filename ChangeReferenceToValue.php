1995772bdb675aafc306c9203ca23ab7fb7196be
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-08-22 10:25:04 +0200

    Simplified implementation to reach Value Object

--- ChangeReferenceToValue.php
+++ ChangeReferenceToValue.php
@@ -4,7 +4,6 @@ class Color
     private $red;
     private $green;
     private $blue;
-    private static $colors = array();
 
     private function __construct($r, $g, $b)
     {
@@ -13,19 +12,9 @@ class Color
         $this->blue = $b;
     }
 
-    /**
-     * A static lookup method is common for the Flyweight pattern 
-     * implementation, and keeps only one instance of each color around.
-     * An equivalent starting point would be a ColorRepository or ColorFactory
-     * class.
-     */
     public static function getColor($r, $g, $b)
     {
-        $code = $r . $g . $b;
-        if (!isset(self::$colors[$code])) {
-            self::$colors[$code] = new self($r, $g, $b);
-        }
-        return self::$colors[$code];
+        return new self($r, $g, $b);
     }
 
     /* ... other methods ... */
