844dff2d4f329ce876a0f8e904f52327f3c57b32
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-08-30 10:18:44 +0200

    Initial state

---
+++ ChangeBidirectionalAssociationToUnidirectional.php
@@ -0,0 +1,66 @@
+<?php
+class ChangeBidirectionalAssociationToUnidirectional extends PHPUnit_Framework_TestCase
+{
+    public function testPhonenumbersDoNotNeedToReferToUsers()
+    {
+        $user = new User('Giorgio');
+        $user->addPhonenumber(new Phonenumber('012345'));
+        $this->assertEquals("012345\n", $user->phonenumbersList());
+    }
+}
+
+class User
+{
+    private $name;
+    private $phonenumbers;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function addPhonenumber(Phonenumber $phonenumber)
+    {
+        $this->phonenumbers[] = $phonenumber;
+        $phonenumber->internalSetUser($this);
+    }
+
+    public function phonenumbersList()
+    {
+        $list = '';
+        foreach ($this->phonenumbers as $number) {
+            $list .= "$number\n";
+        }
+        return $list;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+
+class Phonenumber
+{
+    private $number;
+
+    /**
+     * @var User
+     */
+    private $user;
+
+    public function __construct($number)
+    {
+        $this->number = $number;
+    }
+
+    public function internalSetUser(User $u)
+    {
+        $this->user = $u;
+    }
+
+    public function __toString()
+    {
+        return $this->number;
+    }
+}
