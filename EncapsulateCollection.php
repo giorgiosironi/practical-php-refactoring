9b07c428e78c09f15fe48cd94e56fbaa515e9e00
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-09-12 10:16:48 +0200

    Initial state

---
+++ EncapsulateCollection.php
@@ -0,0 +1,43 @@
+<?php
+class EncapsulateCollection extends PHPUnit_Framework_TestCase
+{
+    public function testCollectionCanBePopulatedAndInspected()
+    {
+        $user = new User();
+        $groups = new ArrayObject(array(
+            new Group('sysadmins'),
+            new Group('developers'),
+            new Group('economists')
+        ));
+        $user->setGroups($groups);
+        $this->assertEquals(3, count($user->getGroups()));
+    }
+}
+
+class User
+{
+    private $groups;
+
+    public function setGroups(ArrayObject $groups)
+    {
+        $this->groups = $groups;
+    }
+
+    public function getGroups()
+    {
+        return $this->groups;
+    }
+}
+
+/**
+ * A simple Value Object in this example, little more than a string.
+ */
+class Group
+{
+    private $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+}
