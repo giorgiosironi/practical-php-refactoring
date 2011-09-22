69e7eedd0879954cc3e5adcc87a068d1595cb9f5
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-09-22 10:04:29 +0200

    Added subclasses; moved creation logic into Factory Method

--- ReplaceTypeCodeWithSubclasses.php
+++ ReplaceTypeCodeWithSubclasses.php
@@ -3,13 +3,13 @@ class ReplaceTypeCodeWithSubclasses extends PHPUnit_Framework_TestCase
 {
     public function testAnUserCanBeANewbie()
     {
-        $user = new User("Giorgio", User::NEWBIE);
+        $user = User::newUser("Giorgio", User::NEWBIE);
         $this->assertEquals("Giorgio", $user->__toString());
     }
 
     public function testAnUserCanBeRegardedAsAGuru()
     {
-        $user = new User("Giorgio", User::GURU);
+        $user = User::newUser("Giorgio", User::GURU);
         $this->assertEquals("ADMIN: Giorgio", $user->__toString());
     }
 }
@@ -19,13 +19,21 @@ class User
     const NEWBIE = 'N';
     const GURU = 'G';
 
-    private $name;
+    protected $name;
     private $rank;
 
     public function __construct($name, $rank)
     {
         $this->name = $name;
         $this->rank = $rank;
+    }
+
+    public static function newUser($name, $rank)
+    {
+        if ($rank == self::GURU) {
+            return new Guru($name, null);
+        }
+        return new Newbie($name, null);
     }
 
     protected function getRank()
@@ -40,5 +48,21 @@ class User
         }
         // self::NEWBIE
         return $this->name;
+    }
+}
+
+class Guru extends User
+{
+    protected function getRank()
+    {
+        return self::GURU;
+    }
+}
+
+class Newbie extends User
+{
+    protected function getRank()
+    {
+        return self::NEWBIE;
     }
 }
