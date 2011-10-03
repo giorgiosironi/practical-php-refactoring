5b0fead6c9a06971851c2ea725e8d8cc86cfd7cd
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-10-03 09:47:20 +0200

    Initial state

---
+++ DecomposeConditional.php
@@ -0,0 +1,44 @@
+<?php
+class DecomposeConditional extends PHPUnit_Framework_TestCase
+{
+    public function testAGroupIsInEvidenceWhenManyPostsArePresent()
+    {
+        $group = new Group('PHP forum', 100);
+        $this->assertEquals('<span class="evidence">In evidence: PHP forum</span>', $group->__toString());
+    }
+
+    public function testAGroupIsShownAsNormalWhenThereAreNotManyPosts()
+    {
+        $group = new Group('PHP forum', 10);
+        $this->assertEquals('<span>PHP forum</span>', $group->__toString());
+    }
+
+    public function testAGroupIsAlsoInEvidenceWhenItHasBeenRecentlyCreated()
+    {
+        $group = new Group('PHP forum', 0, true);
+        $this->assertEquals('<span class="evidence">In evidence: PHP forum</span>', $group->__toString());
+    }
+}
+
+class Group
+{
+    private $name;
+    private $posts;
+    private $recentlyCreated;
+
+    public function __construct($name, $posts, $recentlyCreated = false)
+    {
+        $this->name = $name;
+        $this->posts = $posts;
+        $this->recentlyCreated = $recentlyCreated;
+    }
+
+    public function __toString()
+    {
+        if ($this->posts > 50 || $this->recentlyCreated) {
+            return "<span class=\"evidence\">In evidence: $this->name</span>";
+        } else {
+            return "<span>$this->name</span>";
+        }
+    }
+}
