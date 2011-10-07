fe5dd6f8ac7118c83ebcbe235ebd2c723f79ca19
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-10-07 09:51:20 +0200

    Initial state

---
+++ ConsolidateDuplicateConditionalFragments.php
@@ -0,0 +1,40 @@
+<?php
+class ConsolidateDuplicateConditionalFragments extends PHPUnit_Framework_TestCase
+{
+    public function testTotalPaymentIncludeTaxesAndProcessingFee()
+    {
+        $invoice = new Invoice(990, 21, false);
+        $this->assertEquals(1210, $invoice->getTotal());
+    }
+
+    public function testTotalCanBeDiscountedBeforeTaxes()
+    {
+        $invoice = new Invoice(1250, 21, 20);
+        $this->assertEquals(1210, $invoice->getTotal());
+    }
+}
+
+class Invoice
+{
+    private $taxable;
+    private $taxRate;
+    private $discount;
+    const PROCESSING_FEE = 10;
+
+    public function __construct($taxable, $taxRate, $discount = false)
+    {
+        $this->taxable = $taxable;
+        $this->taxRate = $taxRate;
+        $this->discount = $discount;
+    }
+
+    public function getTotal()
+    {
+        if ($this->discount) {
+            $total = $this->taxable * (1 - $this->discount / 100);
+            return $total * (1 + $this->taxRate / 100);
+        } else {
+            return ($this->taxable + self::PROCESSING_FEE) * (1 + $this->taxRate / 100);
+        }
+    }
+}
