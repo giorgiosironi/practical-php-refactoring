20568dd8a5eea4bcb6f6e13b5975e35a590b60fb
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-10-12 14:58:04 +0200

    Initial state

---
+++ RemoveControlFlag.php
@@ -0,0 +1,35 @@
+<?php
+class RemoveControlFlag extends PHPUnit_Framework_TestCase
+{
+    public function testFindsTheUserWhoseNameIsShortEnough() {
+        $users = new Users(array('Giorgio', 'John', 'Tim'));
+        $user = $users->findUserWithNameAsShortAs(3);
+        $this->assertEquals('Tim', $user);
+    }
+}
+
+class Users
+{
+    private $users;
+
+    public function __construct(array $users)
+    {
+        $this->users = $users;
+    }
+
+    public function findUserWithNameAsShortAs($length)
+    {
+        $found = false;
+        $i = 0;
+        $length = count($this->users);
+        while ($i < $length && !$found) {
+            $user = $this->users[$i];
+            if (strlen($user) == 3) {
+                $found = true;
+            }
+            $i++;
+        }
+        return $user;
+    }
+}
+
