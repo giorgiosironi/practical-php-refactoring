66b01a6f04a29b0ecec99ebeee5b1d9502c6e8c4
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-10-13 13:47:56 +0200

    Initial state

---
+++ ReplaceNestedConditionalWithGuardClauses.php
@@ -0,0 +1,49 @@
+<?php
+class ReplaceNestedConditionalWithGuardClauses extends PHPUnit_Framework_TestCase
+{
+    public function testTheOpenTopicTitleIsDisplayedNormally()
+    {
+         $topic = new Topic("Hello");
+         $this->assertEquals("Hello", $topic->__toString());
+    }
+
+    public function testTheClosedTopicTitleIsDisplayedWithACorrespondingIndication()
+    {
+         $topic = new Topic("Hello", true);
+         $this->assertEquals("Closed: Hello", $topic->__toString());
+    }
+
+    public function testTheClosedTopicTitleIsDisplayedNormallyToAdmins()
+    {
+         $topic = new Topic("Hello", true, true);
+         $this->assertEquals("Closed (not for you): Hello", $topic->__toString());
+    }
+}
+
+class Topic
+{
+    private $title;
+    private $isClosed;
+    private $isAdminViewing;
+
+    public function __construct($title, $isClosed = false, $isAdminViewing = false)
+    {
+        $this->title = $title;
+        $this->isClosed = $isClosed;
+        $this->isAdminViewing = $isAdminViewing;
+    }
+
+    public function __toString()
+    {
+        if (!$this->isClosed) {
+            $displayed = $this->title;
+        } else {
+            if ($this->isAdminViewing) {
+                $displayed = "Closed (not for you): $this->title";
+            } else {
+                $displayed = "Closed: $this->title";
+            }
+        }
+        return $displayed;
+    }
+}
