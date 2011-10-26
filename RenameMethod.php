29f9df5cfa9a8b95a4dd1490510ec780f829b920
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-10-26 14:38:51 +0200

    Initial state

---
+++ RenameMethod.php
@@ -0,0 +1,18 @@
+<?php
+class RenameMethod extends PHPUnit_Framework_TestCase
+{
+    public function testSimulatesClientCode()
+    {
+        $repository = new UserRepository();
+        $this->assertEquals(0, $repository->snafucatedUsers());
+    }
+}
+
+class UserRepository
+{
+    public function snafucatedUsers()
+    {
+        // ...complex calculations...
+        return 0;
+    }
+}
