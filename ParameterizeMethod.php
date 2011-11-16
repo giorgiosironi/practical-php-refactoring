ef6eaa5569b280d069085f9be4783d18c9898a15
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-11-16 10:09:24 +0100

    Initial state

---
+++ ParameterizeMethod.php
@@ -0,0 +1,37 @@
+<?php
+class ParameterizeMethod extends PHPUnit_Framework_TestCase
+{
+    public function testTheArticleIsConsideredPopularAfter1000Views()
+    {
+        $article = new Article('PPR: Extract Method', 1000);
+        $this->assertTrue($article->isPopular());
+    }
+
+    public function testTheArticleIsConsideredInTheTopRankAfter10000Views()
+    {
+        $article = new Article('How to be a worse programmer', 10000);
+        $this->assertTrue($article->isTop());
+    }
+}
+
+class Article
+{
+    private $title;
+    private $views;
+
+    public function __construct($title, $views)
+    {
+        $this->title = $title;
+        $this->views = $views;
+    }
+
+    public function isPopular()
+    {
+        return $this->views >= 1000;
+    }
+
+    public function isTop()
+    {
+        return $this->views >= 10000;
+    }
+}
