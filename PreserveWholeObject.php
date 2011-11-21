f2f78fdff43a65cc7bfadb5b84a7547b93f8fd85
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-11-21 18:48:43 +0100

    Initial state

---
+++ PreserveWholeObject.php
@@ -0,0 +1,20 @@
+<?php
+class PreserveWholeObject extends PHPUnit_Framework_TestCase
+{
+    public function testTheSlotEvaluatesItsLength()
+    {
+        $today = new DateTime('2011-11-23');
+        $slot = new MonthSpecificSlot();
+        $this->assertTrue($slot->containsAWeek($today->format('m'), $today->format('d')));
+    }
+}
+
+class MonthSpecificSlot
+{
+    public function containsAWeek($month, $day)
+    {
+        $reference = new DateTime('2011-' . $month . '-01');
+        $daysInMonth = $reference->format('t');
+        return $day + 6 <= $daysInMonth;
+    }
+}
