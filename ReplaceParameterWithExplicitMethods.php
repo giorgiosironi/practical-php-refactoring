ee2e1cc725879d4e5abce820f0abc366d907167c
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-11-21 18:31:21 +0100

    Initial state

---
+++ ReplaceParameterWithExplicitMethods.php
@@ -0,0 +1,50 @@
+<?php
+class ReplaceParameterWithExplicitMethods extends PHPUnit_Framework_TestCase
+{
+    public function testThreadCanBeClosed()
+    {
+        $thread = new Thread('Ubuntu on EEE Pc');
+        $thread->setOpen(false);
+        $this->assertFalse($thread->getOpen());
+        $this->assertEquals('[closed] Ubuntu on EEE Pc', $thread->__toString());
+    }
+
+    public function testThreadCanBeOpened()
+    {
+        $thread = new Thread('Ubuntu on EEE Pc');
+        $thread->setOpen(true);
+        $this->assertTrue($thread->getOpen());
+        $this->assertEquals('Ubuntu on EEE Pc', $thread->__toString());
+    }
+}
+
+class Thread
+{
+    private $title;
+    private $open;
+
+    public function __construct($title)
+    {
+        $this->title = $title;
+    }
+
+    public function setOpen($state)
+    {
+        $this->open = $state;
+        if (!$this->open) {
+            $this->label = '[closed] ' . $this->title;
+        } else {
+            $this->label = $this->title;
+        }
+    }
+
+    public function getOpen()
+    {
+        return $this->open;
+    }
+
+    public function __toString()
+    {
+        return $this->label;
+    }
+}
