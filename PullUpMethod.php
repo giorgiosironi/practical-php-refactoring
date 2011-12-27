bf9c8d64ae28f7d2d36ea536913f0b8149354ed2
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-12-27 09:10:42 +0100

    Initial state

---
+++ PullUpMethod.php
@@ -0,0 +1,57 @@
+<?php
+class PullUpMethod extends PHPUnit_Framework_TestCase
+{
+    public function testAPostShowsItsAuthor()
+    {
+        $post = new Post("Hello, world!", "giorgiosironi");
+        $this->assertEquals("Hello, world! -- giorgiosironi",
+                            $post->__toString());
+    }
+
+    public function testALinkShowsItsAuthor()
+    {
+        $link = new Link("http://en.wikipedia.com", "giorgiosironi");
+        $this->assertEquals("<a href=\"http://en.wikipedia.com\">http://en.wikipedia.com</a> -- giorgiosironi",
+                            $link->__toString());
+    }
+}
+
+abstract class NewsFeedItem
+{
+    /**
+     * @var string  references the author's Twitter username
+     */
+    protected $author;
+}
+
+class Post extends NewsFeedItem
+{
+    private $text;
+
+    public function __construct($text, $author)
+    {
+        $this->text = $text;
+        $this->author = $author;
+    }
+
+    public function __toString()
+    {
+        return "$this->text -- $this->author";
+    }
+}
+
+class Link extends NewsFeedItem
+{
+    private $url;
+
+    public function __construct($url, $author)
+    {
+        $this->url = $url;
+        $this->author = $author;
+    }
+
+    public function __toString()
+    {
+        return "<a href=\"$this->url\">$this->url</a> -- $this->author";
+    }
+}
